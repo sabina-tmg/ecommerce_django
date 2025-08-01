16a16db8d1d50a5f4b799690e58e67c369f8c0b0
sabina tamang <syangtansabia8@gmail.com>
2025-08-01 08:01:02 +0545

    this is the first commit

---
+++ store/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from category.models import Category
+from django.db.models import Q
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    single_product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+    
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
